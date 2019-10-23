6a8240eb7b9a142ef72b9fe9a5fe4f954a190ccb
Khalid Sultan <kalid.sultan.95@gmail.com>
2019-10-23 23:48:06 +0300

    Added A Weekly Prediction Base and Graph

--- app.py
+++ app.py
@@ -1,17 +1,12 @@
+import json
+import time
+
+import pandas as pd
+import requests
 from flask import Flask, render_template
-from flask import request
 from flask import json
 from flask import jsonify
-
-import requests
-import time
-import datetime
-import json
-
-
-import numpy as np
-import pandas as pd
-
+from flask import request
 from keras.models import load_model
 
 app = Flask(__name__)
@@ -36,6 +31,7 @@ def generate():
     dewPoint = variables['dewPoint']
     stationPressure = variables['stationPressure']
     altimeter = variables['altimeter']
+
     given_values = {
         'Cloud Coverage': [cloudCoverage],
         'Visibility': [visibility],
@@ -56,7 +52,68 @@ def generate():
 
 
 @app.route("/generateTimeline")
-def generate_timeline():
+def generateTimeline():
+    print("Loading Keras Model")
+    weather = get_current_location_weather_week()
+    keys = ['cloudCover', 'visibility', 'humidity', 'windSpeed',
+            'temperature', 'dewPoint', 'pressure', 'pressure']
+    daily = weather['daily']['data']
+    datas = []
+    datekeys = {}
+    print(daily)
+    for variables in daily:
+        date = time.strftime('%Y-%m-%d', time.localtime(variables['time']))
+        datekeys[date] = 1
+
+        cloudCoverage = variables['cloudCover']
+        visibility = variables['visibility']
+        relativeHumidity = variables['humidity']
+        windSpeed = variables['windSpeed']
+        temperature = (variables['temperatureMax'] + variables['temperatureMin']) / 2
+        dewPoint = variables['dewPoint']
+        stationPressure = variables['pressure']
+        altimeter = variables['pressure']
+        given_values = {
+            'Cloud Coverage': [cloudCoverage],
+            'Visibility': [visibility],
+            'Temperature': [relativeHumidity],
+            'Dew Point': [windSpeed],
+            'Relative Humidity': [temperature],
+            'Wind Speed': [dewPoint],
+            'Station Pressure': [stationPressure],
+            'Altimeter': [altimeter]
+        }
+        clean_values = pd.DataFrame(given_values)
+        clean_values = clean_values.astype('float32')
+        datas.append(clean_values)
+    df = pd.concat(datas)
+    results = {}
+    model = get_model()
+    predictions = model.predict(df)
+    counter = 0
+    for i in predictions[0:]:
+        savant = 0
+        key = ""
+        for j in datekeys.keys():
+            if counter == savant:
+                key = j
+                break
+            savant += 1
+        counter += 1
+        results[key] = '{}'.format(i[0])
+    json_results = []
+    counter = 0
+    for i in results.keys():
+        json_results_temp = {'date': i, 'value': float(results[i]), 'max': max(results.values()),
+                             'min': min(results.values())}
+        counter += 1
+        json_results.append(json_results_temp)
+    print(json_results)
+    return jsonify(json_results)
+
+
+@app.route("/compare")
+def compare():
     print("Loading Keras Model")
     countries = get_countries()
     datas = []
@@ -108,7 +165,8 @@ def generate_timeline():
         results[key] = '{}'.format(i[0])
     json_results = []
     for i in results.keys():
-        json_results_temp = {'Country': i, 'Value': results[i]}
+        json_results_temp = {'Country': i, 'Value': results[i], 'max': max(results.values()),
+                             'min': min(results.values())}
         json_results.append(json_results_temp)
     return jsonify(json_results)
 
@@ -117,9 +175,18 @@ def generate_timeline():
 def get_current_location_weather_now():
     latitude, longitude = get_my_current_location()
     url = 'https://api.darksky.net/forecast/{}/{},{}'.format(api_key, latitude, longitude)
+    # url = 'https://api.openweathermap.org/data/2.5/weather?lat={}&lon={}&appid='.format(latitude,longitude,api_key)
     r = requests.get(url)
     j = json.loads(r.text)
     return jsonify(j)
+
+
+def get_current_location_weather_week():
+    latitude, longitude = get_my_current_location()
+    url = 'https://api.darksky.net/forecast/{}/{},{}'.format(api_key, latitude, longitude)
+    r = requests.get(url)
+    j = json.loads(r.text)
+    return j
 
 
 def get_location_weather_now(latitude, longitude):
